7a3eaffd9fc737e9b2983ff98961da1d8dd0a446
suki <echo_c120@163.com>
2021-05-31 20:38:41 +0800

    delete mode 100644 management/art-template346250241346235277345274225346223216346224271345206231347232204347275221347253231/node_modules/mongoose/lib/schema/objectid.js

---
+++ demo-1-(爱马仕官网)/js/menu.js
@@ -0,0 +1,54 @@
+let username = $('.username-block')
+$('.login-registered>a p').click(function() {
+    $('.menu-btn-say').parent().parent().animate({
+        width: 500,
+    }, 'linear')
+})
+$('.menu-btn-say').click(function() {
+    $('.menu-btn-say').parent().parent().animate({
+        width: 0,
+    }, 'linear')
+})
+$('.menu-btn-login').click(function() {
+    $.post("http://localhost/src/php/login.php", {
+            account: ($('#menu .menu-content [name="account"]').val()),
+            password: ($('#menu .menu-content [name="password"]').val()),
+        },
+        function(res) {
+            let { meta: { status, message }, date } = JSON.parse(res)
+            if (status == 1) {
+                $.cookie("setname", date)
+                username.css("display", "block")
+                $('.username-block button span a p').html(date)
+                $('.username-block span a p').text(date)
+                    // alert(message)
+                layer.alert(message, {
+                    icon: 1,
+                    skin: 'layer-ext-demo'
+                })
+                $('.menu-btn-say').parent().parent().animate({
+                    width: 0,
+                }, 'linear')
+
+            } else if (status == 0) {
+                layer.alert(message, {
+                    icon: 2,
+                    skin: 'layer-ext-demo'
+                })
+            }
+        },
+        "text"
+    )
+})
+if ($.cookie("setname") != null) {
+    username.css("display", "block")
+    $('.username-block span a p').text($.cookie("setname"))
+}
+$('.username-block button').click(function() {
+    if (!confirm('确定要退出该账户嘛')) {
+        return false
+    }
+    $.removeCookie("setname")
+    username.css("display", "none")
+    layer.msg('再见了您嘞', { icon: 5 });
+})
